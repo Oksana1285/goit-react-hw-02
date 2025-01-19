5260c593d8c7ebd757bae237a90b18827d183a57
Oksana Kolesova <kolesovaok85@gmail.com>
2025-01-19 12:08:23 +0200

    hw2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,63 @@
-import Profile from "./components/Profile/Profile";
-import FriendList from "./components/FriendList/FriendList";
+import css from './App.module.css';
 
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import Description from './components/Description/Description';
+import Feedback from './components/Feedback/Feedback';
+import Notification from './components/Notification/Notification';
+import Option from './components/Options/Options';
 
-import friends from "./Data/friends.json";
-import transactions from "./Data/transactions.json";
-import userData from "./Data/userData.json";
+import { useState, useEffect } from 'react';
+
+const reviewsType = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+
+const savedFeedback = () => {
+  return window.localStorage.getItem('feedback-data') !== null
+    ? JSON.parse(localStorage.getItem('feedback-data'))
+    : reviewsType;
+};
 
 const App = () => {
+  const [feedback, setFeedback] = useState(savedFeedback);
+
+  useEffect(() => {
+    window.localStorage.setItem('feedback-data', JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = feedbackType => {
+    if (feedbackType === 'reset') {
+      setFeedback(reviewsType);
+    }
+    setFeedback({ ...feedback, [feedbackType]: feedback[feedbackType] + 1 });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = Math.round(
+    ((feedback.good + feedback.neutral) / totalFeedback) * 100
+  );
+
+  const resetFeedbackBtn = () => setFeedback(reviewsType);
+
   return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
+    <div className={css.container}>
+      <Description />
+      <Option
+        onClickFeedback={feedbackType => updateFeedback(feedbackType)}
+        resetFeedback={totalFeedback >= 1}
+        resetBtn={resetFeedbackBtn}
       />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </>
+      {totalFeedback >= 1 && (
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      )}{' '}
+      {totalFeedback < 1 && <Notification />}
+    </div>
   );
 };
+
 export default App;
